d2d05b559b9c818b5c81b0b71df9ad5741d60f2b
Maxim <slastonm@gmail.com>
2025-01-14 21:01:29 +0200

    01
    CW 01

---
+++ 01/scripts/StudyMy.js
@@ -0,0 +1,90 @@
+
+// var bodyColor = "white";
+
+let bodyColor = "white";
+let BodyColor ='black';
+let body_color = 'red';
+let bodycolor = 'green';
+
+// const fonSize = 16;
+
+// не можна
+// let body color = 'blue';
+// let 1bodyColor = 'yellow';
+// let !bodyColor = 'orange';
+// let "bodyColor = 'orange';
+// let body-Color = 'orange'; // - це знак мінус
+// let const = 15;
+
+
+let userName = "John";
+// alert(userName);
+console.log(userName);
+
+let firstNumber = '10';
+let secondNumber = "20";
+
+let sum = firstNumber + secondNumber;
+// alert(sum);
+console.log(sum);
+
+//String
+let string = "Hello World";
+let productDescription = "Product's description iphone 17 pro max price 9999$";
+let mainTitle = 'Main Page';
+
+//Number
+
+let price = 1000;
+let productPrice = 9.99;
+let PiNumber = 3.1432143242314123;
+let saleVvalue = 0.1;
+let negativeNumber = -10;
+let NotANumber = NaN; 
+let GetNan = 10 / "a";
+console.log(GetNan);
+let infinity = 1 / 0;
+console.log(infinity);
+let negativeInfinity = -1 / 0;
+console.log(negativeInfinity);
+
+//Boolean
+let isOnline = true;
+let isModalOpen = false;
+
+// let isOnline = true; це вже не буліан
+
+//Undefined
+let firstName;
+console.log(firstName);
+
+//Null
+let user = null;
+
+// let firstNumber = 100;
+
+let anontherValue = 'Test';
+
+let numberFirst = 100;
+// numberFirst = 99;
+// console.log(numberFirst);
+// numberFirst = '98 poducts';
+// numberFirst = true;
+numberFirst = anontherValue;
+
+console.log(numberFirst); // 'Test'
+
+// Оголосіть дві змінні: userAgeValue з числовим значенням та userNameNickname з значенням текстовим.
+// Змініть значення userAgeValue та задайте нове значення userNameNickname
+
+let userAgeValue = 26;
+let userNameNickname = 'John';
+
+userAgeValue = 30;
+userNameNickname = 'Alex';
+
+
+
+
+
+
